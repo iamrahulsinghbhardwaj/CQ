6f4317ef35e27d31f9426b719b7d2effd4c9ba55
Rahul Singh <42406373+iamrahulsinghbhardwaj@users.noreply.github.com>
2020-04-14 19:30:18 +0530

    Update Intersection of Two Arrays.java

--- Two pointers/Intersection of Two Arrays.java
+++ Two pointers/Intersection of Two Arrays.java
@@ -1,1 +1,26 @@
+/*
+Problem URL https://leetcode.com/problems/intersection-of-two-arrays/
+*/
 
+// find common digits
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> first=new HashSet<>();
+        HashSet<Integer> last=new HashSet<>();
+        for(int x: nums1){
+            first.add(x);
+        }
+        for(int x: nums2){
+            if(first.contains(x)){
+                last.add(x);
+            }
+        }
+        int index=0;
+        int a[]=new int[last.size()];
+        for(int x: last){
+            a[index++]=x;
+        }
+        return a;
+    }
+}
